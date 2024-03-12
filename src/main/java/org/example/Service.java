13c24231b7e5fcfe109bd1262ffa18eb83abcb0b
olafenko <olopikolo666@gmail.com>
2024-03-12 17:12:19 +0100

    c

--- src/main/java/org/example/Service.java
+++ src/main/java/org/example/Service.java
@@ -3,12 +3,16 @@ package org.example;
 import org.example.dto.PlaceDetails;
 import org.example.model.Result;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpRequest;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Scanner;
 
 public class Service {
-
+    
     private List<PlaceDetails> placeDetailsList = new ArrayList<>();
 
     public void getPlaceDetails(List<Result> results) {
@@ -50,6 +54,41 @@ public class Service {
             }
         }
         return String.valueOf(linkChars);
+
+    }
+
+    public HttpRequest buildRequest(Scanner scanner) throws URISyntaxException {
+
+
+
+        //required
+        String location ="50.052226541336964,21.604342249873344";
+        //required
+        Integer radius = 1000;
+        String opennow ="opennow=true";
+        String type ="type=";
+        String keyword="keyword=";
+
+        String link = String.format("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=%l&raduis=%r",location,radius);
+
+
+        do {
+            System.out.println("MENU");
+            System.out.println("1. Change radius.");
+            System.out.println("2. Change type.");
+            System.out.println("3. Change open now filter.");
+            System.out.println("4. Add keyword.");
+            System.out.println("5. Send request");
+            System.out.println();
+            scanner.nextInt();
+
+        } while (scanner.nextInt() != 0);
+
+
+
+
+
+        return HttpRequest.newBuilder().uri(new URI(link)).GET().build();
 
     }
 }
